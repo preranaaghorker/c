2de5c161d5803410ac6dba40d97b70595932e3e6
preranaaghorker <aghorker.pre@gmail.com>
2018-01-08 09:28:19 +0530

    Added C_Lec files

---
+++ size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("size of char is=%d\n",sizeof(char));
+	printf("size of int is=%d\n",sizeof(int));
+	printf("size of float is=%d\n",sizeof(float));
+	printf("size of double is=%d\n",sizeof(double));
+	return 0;
+}
---
+++ size1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	for(ch=0;ch<260;ch++)
+	{
+		printf("%d\n",ch);
+	}
+	return 0;
+}
